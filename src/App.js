3a1cfc80ad16bd7ad518282c8acf728f80fce73b
Paul Jeon <pauljeonn@gmail.com>
2021-11-03 15:21:14 +0900

    create Home page component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Home from './pages/Home';
+
 function App() {
-	return <div className="App">Ecommerce App</div>;
+	return (
+		<div className="App">
+			<Home />
+		</div>
+	);
 }
 
 export default App;
